3ccbac5cd35044f60d528e8726f8bdeec749f29e
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-03-23 13:00:01 -0700

    Validar usuario

--- public/index.php
+++ public/index.php
@@ -6,3 +6,19 @@ $usuario = new \Bidkar\Brawlhalla\Libs\User();
 $usuario->uuid = \Ramsey\Uuid\v4();
 
 echo $usuario->uuid;
+
+var_dump($usuario->validateName('bidkar4jhskjd867yuweyiwe'));
+$usuario->setName('bidkar3jkkhs');
+echo $usuario->getName();
+
+echo '<br>Contraseña: ';
+$pwd = 'kldfRjf34lks';
+var_dump($usuario->validatePassword($pwd));
+
+echo '<br>Email: ';
+$email = 'bidkar@gmail.com';
+var_dump($usuario->validateEmail($email));
+if ($usuario->validateEmail($email))
+    echo "$email correcto";
+else
+    echo "$email incorrecto";
--- src/libs/User.php
+++ src/libs/User.php
@@ -1,13 +1,42 @@
 <?php
+
 namespace Bidkar\Brawlhalla\Libs;
 
 class User
 {
     public $id;
     public $uuid;
-    public $name;
-    public $password;
-    public $email;
+    protected $name;
+    protected $password;
+    protected $email;
     public $created_at;
     public $status; // 1=Active 0=Inactive
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($value)
+    {
+        $this->name = $value;
+    }
+
+    public function validateName($name)
+    {
+        # entre 8 y 16 caracteres
+        # solo letras minusculas y números
+        return (bool) preg_match("/^[a-z0-9]{8,16}$/", $name);
+    }
+
+    public function validatePassword($password)
+    {
+        $regex = '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([A-Za-z0-9]){6,12}$/';
+        return (bool) preg_match($regex, $password);
+    }
+
+    public function validateEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
 }
